463f72e4bf254ed2268d1a9453e583e5c8c2ef17
Quantre34 <93018221+Quantre34@users.noreply.github.com>
2022-01-12 09:28:27 +0300

    Add files via upload

---
+++ speller.js
@@ -0,0 +1,88 @@
+
+function spellcheck(text){
+    const arr = [
+"a",
+"a / e",
+"ab",
+"aba",
+"abaci",
+"abacilik",
+"abadi",
+"aba guresi",
+"abajur",
+"abajurcu",
+"abajurculuk",
+"abajurlu",
+"abajursuz",
+"abakus",
+"abali",
+"Abana",
+"abanabilme",
+"abanabilmek",
+"abandirabilme",
+"abandirabilmek",
+"abandiriverme",
+"abandirivermek",
+"abandirma",
+"abandirmak",
+"abandone",
+"abanis",
+"abaniverme",
+"abanivermek",
+"abani",
+"abanma",
+"abanmak",
+"abanoz",
+"abanozgiller",
+"abanozlasiverme",
+"abanozlasivermek",
+"abanozlasma",
+"abanozlasmak",
+"abanozlastirabilme",
+"abanozlastirabilmek"
+
+];
+
+///let  text = "yonetsea";
+let result = arr.map(function(row, index){
+    var res;
+    if(row.trim() == text.trim()){
+        return true;
+    }else {
+        return false;      
+    }
+});
+if (result.includes(true)){
+    return true;
+}else {
+        const apr = [];
+        let suggestions = [];
+        arr.forEach(function(row, index){
+            let x = row.split("");
+            let swing = text.trim();
+            let y = swing.split("");
+            let percentage = row.length;/// this means whole
+            let range = 0; /// this is going to show how much letters gonna match
+            x.forEach(function(b, index){
+                 if(b == y[index]){
+                     range += 1;
+                 }
+            });
+            let data = [
+                (percentage / range), 
+                row
+            ];
+            apr.push(data); 
+        });
+        let list = apr.sort(function(a,b){
+            return a-b;
+        });
+        let similars = [];
+        list.forEach(function(row, index){
+        if(row[0] <= 1.3){
+            similars.push(row);
+        }
+        });
+        return similars;
+}
+}
